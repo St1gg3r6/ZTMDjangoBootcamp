0e2f5cf0edc21527aa782087dce960757705b628
stephenusher <steveusher34@btinternet.com>
2024-12-17 11:27:56 +0000

    Rename project directory

--- Project 2 Favourite Movies/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-cd 'Project 2 Favourite Movies'
-
-pip install -r requirements.txt
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
